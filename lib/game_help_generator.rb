155fb1148f79d090ae04f7c4abdd265a9b9038e8
Vadim <vadimsorella@gmail.com>
2023-06-11 14:35:33 +0600

    refactor tests

--- lib/game_help_generator.rb
+++ lib/game_help_generator.rb
@@ -6,36 +6,40 @@ class GameHelpGenerator
   TOTAL_WATCHERS = 100
   MIN_PROBABILITY = 80
 
-  # Возвращает hash c массивом ключей keys и значениями - распределением в процентах
-  # correct_key - ключ правильного ответа, он будет выбран с бОльшим весом
-  def self.audience_distribution(keys, correct_key)
-    result_array = keys.map do |key|
-      if key == correct_key
-        rand(45..90)
-      else
-        rand(1..60)
+  class << self
+    # Возвращает hash c массивом ключей keys и значениями - распределением в процентах
+    # correct_key - ключ правильного ответа, он будет выбран с бОльшим весом
+    def audience_distribution(keys, correct_key)
+      result_array = keys.map do |key|
+        if key == correct_key
+          rand(45..90)
+        else
+          rand(1..60)
+        end
       end
+
+      # нормализуем массив
+      sum = result_array.sum
+      result_array.map! { |v| TOTAL_WATCHERS * v / sum }
+
+      # возвращаем хэш, собранный из массива ключей и значений (см. доку на метод Array#zip)
+      Hash[keys.zip(result_array)]
     end
 
-    # нормализуем массив
-    sum = result_array.sum
-    result_array.map! { |v| TOTAL_WATCHERS * v / sum }
+    # Возвращает строку подсказки: что советует друг
+    # correct_key - ключ правильного ответа, он будет выбран с бОльшим весом
+    def friend_call(keys, correct_key)
+      # c ~80% вероятностью выбираем правильный ключ, и с 20% - неправильный
+      key = correct_key
+      key = keys.grep_v(correct_key).sample if random_of_hundred > MIN_PROBABILITY
 
-    # возвращаем хэш, собранный из массива ключей и значений (см. доку на метод Array#zip)
-    Hash[keys.zip(result_array)]
-  end
+      I18n.t('game_help.friend_call', name: I18n.t('game_help.friends').sample, variant: key.upcase)
+    end
 
-  # Возвращает строку подсказки: что советует друг
-  # correct_key - ключ правильного ответа, он будет выбран с бОльшим весом
-  def self.friend_call(keys, correct_key)
-    # c ~80% вероятностью выбираем правильный ключ, и с 20% - неправильный
-    key = correct_key
-    key = keys.grep_v(correct_key).sample if random_of_hundred > MIN_PROBABILITY
+    private
 
-    I18n.t('game_help.friend_call', name: I18n.t('game_help.friends').sample, variant: key.upcase)
-  end
-
-  def self.random_of_hundred
-    rand(1..100)
+    def random_of_hundred
+      rand(1..100)
+    end
   end
 end
